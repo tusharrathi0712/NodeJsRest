2f2f12c2242cb2c5b8448610b883283a854d1be5
tusharrathi0712 <tusharrathi0712@gmail.com>
2017-06-20 22:33:05 +0530

    logger module added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var route = require('./rest');
-
+var logger = require('logger');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended : false
